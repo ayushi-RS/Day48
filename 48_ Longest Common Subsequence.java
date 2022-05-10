a448d6ed55bc88aaf448e5dfda24cec290b5c7e4
Ayushi Malviya <102866923+ayushi-RS@users.noreply.github.com>
2022-05-11 00:13:59 +0530

    Add files via upload

---
+++ 48_ Longest Common Subsequence.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int dp[][]=new int[text1.length()+1][text2.length()+1];
+        int k=text2.length();
+        for(int i=0;i<text1.length()+1;i++)
+        {
+            dp[i][k]=0;
+        }
+        k=text1.length();
+        for(int i=0;i<text2.length()+1;i++)
+        {
+            dp[k][i]=0;
+        }
+        
+       
+        for(int i=dp.length-2;i>=0;i--)
+        {
+            for(int j=dp[0].length-2;j>=0;j--)
+            {
+                if(text1.charAt(i)==text2.charAt(j))
+                {
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
